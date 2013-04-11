f3f85527a02494542149aff16b4044b368328227
Dean Sofer <dsofer@paxata.com>
2013-04-10 18:11:23 -0700

    v0.0.1 unstable

---
+++ test/test.conf.js
@@ -0,0 +1,11 @@
+basePath = '..';
+files = [
+  JASMINE,
+  JASMINE_ADAPTER,
+  'components/angular/angular.js',
+  'components/angular-mocks/angular-mocks.js',
+  'src/ace.js',
+  'test/*.js'
+];
+singleRun = true;
+browsers = [ 'Chrome' ];
