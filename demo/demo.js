df6a1b67067105220e7e4b0b600275653a97f47a
Douglas Duteil <douglasduteil@gmail.com>
2013-08-17 17:08:32 +0200

    feat(demo): Add angular-ui-bootstrap-bower for tabset directive.

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,4 @@
-
-angular.module('doc.ui-ace', ['ui.ace', 'prettifyDirective'])
+angular.module('doc.ui-ace', ['ui.ace', 'prettifyDirective', 'ui.bootstrap'])
   .controller('AceCtrl', ['$scope', function ($scope) {
     $scope.aceModel = "Ace Hello World";
   }])
