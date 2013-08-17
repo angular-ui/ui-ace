c139e8002b0768fb986cdd64ff6b1846ed4bc185
Douglas Duteil <douglasduteil@gmail.com>
2013-08-17 17:28:18 +0200

    [D] Update the demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,4 +1,4 @@
-angular.module('doc.ui-ace', ['ui.ace', 'prettifyDirective', 'ui.bootstrap'])
+angular.module('doc.ui-ace', ['ui.ace', 'prettifyDirective', 'ui.bootstrap', 'plunker'])
   .controller('AceCtrl', ['$scope', function ($scope) {
     $scope.aceModel = "Ace Hello World";
   }])
