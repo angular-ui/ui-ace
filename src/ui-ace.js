6835a49707c740b08a3d55d6a8e519676103db0a
Ivan Tanev <vankata.t@gmail.com>
2015-09-12 17:29:05 +0300

    Do not short-circuit updateOptions() on first call from $watch
    In some circumstances, the value of `scope.$eval(attrs.uiAce)` will
    change between the execution of our link function and the first time
    `scope.$watch(attrs.uiAce)` is triggered. Since this is
    context-dependent, we cannot predict it. We could manually store
    "currentOptions" only for the purpose of short-circuiting that first
    call, but that would be a waste of effort, so just allow it to execute
    every time.

--- src/ui-ace.js
+++ src/ui-ace.js
@@ -275,9 +275,8 @@ angular.module('ui.ace', [])
         }
 
         // Listen for option updates
-        var updateOptions = function (current, previous) {
-          if (current === previous) return;
-          opts = angular.extend({}, options, scope.$eval(attrs.uiAce));
+        var updateOptions = function (newOptions) {
+          opts = angular.extend({}, options, newOptions);
 
           opts.callbacks = [ opts.onLoad ];
           if (opts.onLoad !== options.onLoad) {
