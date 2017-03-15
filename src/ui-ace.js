235efeaf6808222650d7c833ca8cbb5214a198df
Fredrik Holm <fredrik.holm@infolyzer.se>
2017-03-15 16:55:11 +0100

    Check if options are used before using config

--- src/ui-ace.js
+++ src/ui-ace.js
@@ -32,14 +32,15 @@ angular.module('ui.ace', [])
 
       // sets the ace base and/or worker path, 
       // if running from concatenated or minified source
-      var config = window.ace.require('ace/config');
-      if (angular.isDefined(opts.basePath)) {
-        config.set('basePath', opts.basePath);
+      if (angular.isDefined(opts.workerPath) || angular.isDefined(opts.basePath)) {
+        var config = window.ace.require('ace/config');
+        if (angular.isDefined(opts.basePath)) {
+          config.set('basePath', opts.basePath);
+        }
+        if (angular.isDefined(opts.workerPath)) {
+          config.set('workerPath', opts.workerPath);
+        }
       }
-      if (angular.isDefined(opts.workerPath)) {
-        config.set('workerPath', opts.workerPath);
-      }
-
       // ace requires loading
       if (angular.isDefined(opts.require)) {
         opts.require.forEach(function (n) {
